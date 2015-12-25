f819c1e359c99cc53b78d974489238c1770f83c2
Matt Weick <mkweick@gmail.com>
2015-12-24 23:42:44 -0500

    add login logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+    redirect_to root_path if logged_in?
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      login(user)
+      redirect_to root_path
+    else
+      flash.now['alert'] = "Invalid username/password"
+      render :new
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout
+      flash.notice = "You have been logged out"
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+end
