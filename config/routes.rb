be9b79dfdbd2b3f3bb9b2d89a5db7589b0c1148b
Matt Weick <mkweick@gmail.com>
2015-12-24 23:53:31 -0500

    change root to new order

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root  'static_pages#home'
+  root  'orders#new'
   get   '/login', to: 'sessions#new'
   post  '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
