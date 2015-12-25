499e432f01d168354021f6b71a3ad17e4a3199a3
Matt Weick <mkweick@gmail.com>
2015-12-24 23:40:40 -0500

    add site wide auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout
+    session.delete(:user_id)
+  end
 end
