986de5215c5c60bfca34872c104e5e1cee3c3546
Matt Weick <mkweick@gmail.com>
2016-02-15 12:17:06 -0500

    remove Keith

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
 User.create!  first_name: "Dave", last_name: "Smolen", username: "dsmolen",
               password: "smolen", whs_id: "15", active: true
 
-User.create!  first_name: "Keith", last_name: "Malinowski", username: "kmalinowski",
-              password: "malinowski", whs_id: "15", active: true
-
 User.create!  first_name: "Eric", last_name: "Krueger", username: "ekrueger",
               password: "krueger", whs_id: '01', admin: true, active: true
 
